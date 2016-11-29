d974eae57c8ac7bca2d41efa346662d9feafacca
Admin <Admin@Khaled.hsd1.wa.comcast.net>
2016-11-29 13:29:10 -0800

    The new assignment

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,10 +1,17 @@
-$(document).ready(function () {
-    $('.material-button-toggle').click(function () {
+//Navigation bar
+$(document).ready(function() {
+    $('.material-button-toggle').click(function() {
         $(this).toggleClass('open');
         $('.option').toggleClass('scale-on');
     });
 });
 
-$('.navbar-collapse a').click(function(){
+$('.navbar-collapse a').click(function() {
     $(".navbar-collapse").collapse('hide');
 });
+
+
+//Bootstrap Carousel
+$(function() {
+    $('#madinah-carousel').carousel();
+});()
